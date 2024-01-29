38d5f2e7df24588c7a7dc8a28b51c6361d6b0674
Harshad <harshadvarma8@gmail.com>
2024-01-29 11:10:48 -0800

    29th Jan

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'Harshad Varma Sagi | Software Developer',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      'Harshad Varma is a software engineer who specializes in Distributed Systems and Generative AI',
+    siteUrl: 'https://hsagi.netlify.app/', // No trailing slash allowed!
+    // image: '/og.png', // Path to your image you placed in the 'static' folder
+    // twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
