8693af6d89824248b87c41b13b8de441f3d5379b
Harshad <harshadvarma8@gmail.com>
2024-01-29 02:20:07 -0800

    Hmm

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import anime from 'animejs';
 import styled from 'styled-components';
-import { IconLoader } from '@components/icons';
 
 const StyledLoader = styled.div`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -86,27 +85,25 @@ const Loader = ({ finishLoading }) => {
       <Helmet bodyAttributes={{ class: `hidden` }} />
 
       <div className="logo-wrapper">
-      <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
-    <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
-      </g>
-    </g>
-    <text x="25" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas, serif">
-      H
-    </text>
-  </svg>
-        {/* <IconLoader /> */}
+        <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+          <title>Logo</title>
+          <g transform="translate(-8.000000, -2.000000)">
+            <g transform="translate(11.000000, 5.000000)">
+              <polygon
+                id="Shape"
+                stroke="currentColor"
+                strokeWidth="5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                points="39 0 0 22 0 67 39 90 78 68 78 23"
+              />
+            </g>
+          </g>
+          <text x="25" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas, serif">
+            H
+          </text>
+        </svg>
       </div>
-      
     </StyledLoader>
   );
 };
