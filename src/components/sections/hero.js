ee0270379ec3ea9cda6067f2b8a0c73edeb7e2ac
Harshad <harshadvarma8@gmail.com>
2024-03-10 19:16:49 -0700

    Updated AboutMe

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -65,7 +65,8 @@ const Hero = () => {
   const four = (
     <>
       <p>
-      I'm currently a Graduate Student Researcher at the Halicioglu Data Science Institute, UC San Diego, with a passion for leveraging artificial intelligence to solve real-world challenges. My journey in tech began with a strong foundation in computer science from UC San Diego, where I've honed my skills and contributed to various research initiatives.      </p>
+      Welcome to my personal website! I'm Harshad Varma Sagi, a dedicated and innovative computer science professional specializing in machine learning, data science, and software development. Dive into my world of cutting-edge projects, technical skills, and academic journey as I explore the intersections of technology and impact.
+      </p>
     </>
   );
 
